1258aec10dd41f956fc933b00e5a9ad675c13d0d
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-02-16 16:20:24 -0300

    feat(Queue/functions.h): Created functions.h && function init()

---
+++ queue/functions.h
@@ -0,0 +1,4 @@
+void init(Queue *ptrQueue) {
+    ptrQueue->front = -1;
+    ptrQueue->end = -1;
+}
--- queue/header.h
+++ queue/header.h
@@ -1,3 +1,5 @@
 #include "constants.h"
 #include "types.h"
 #include "prototypes.h"
+
+#include "functions.h"
