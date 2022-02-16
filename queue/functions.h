82c17cb5f04655665104ac6e65cf3bd2c6e93c04
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-02-16 16:28:58 -0300

    feat(Queue/functions.h): Function CheckLoad()

--- queue/functions.h
+++ queue/functions.h
@@ -2,3 +2,18 @@ void init(Queue *ptrQueue) {
     ptrQueue->front = -1;
     ptrQueue->end = -1;
 }
+
+int checkLoad(Queue *ptrQueue) {
+    if (ptrQueue->front == -1) {
+        // Is empty
+        return -1;
+
+    } else if (ptrQueue->end == (MAXSIZE - 1)) {
+        // Is full
+        return 1;
+        
+    } else {
+        // Has free space
+        return 0;
+    }
+}
--- queue/prototypes.h
+++ queue/prototypes.h
@@ -1,2 +1,9 @@
 void init(Queue *ptrQueue);
 /* Init creates an empty queue */
+
+int checkLoad(Queue *ptrQueue);
+/* CheckLoad returns:
+    -1 = queue is empty
+    0 = queue still has free space
+    1 = queue is full
+*/
