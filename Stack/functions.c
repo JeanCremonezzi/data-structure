3297482182801944c3c1ea177b275f8caf685263
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-02-17 17:44:13 -0300

    feat(Stack/functions.c): Function CheckLoad()

--- Stack/functions.c
+++ Stack/functions.c
@@ -3,3 +3,18 @@
 void init(Stack *ptrStack) {
     ptrStack->top = -1;
 }
+
+int checkLoad(Stack *ptrStack) {
+    if (ptrStack->top == -1) {
+        // Is empty
+        return -1;
+
+    } else if (ptrStack->top == (MAXSIZE - 1)) {
+        // Is full
+        return 1;
+
+    } else {
+        // Has free space
+        return 0;
+    }
+}
--- Stack/prototypes.h
+++ Stack/prototypes.h
@@ -1,2 +1,9 @@
 void init(Stack *ptrStack);
 /* Init creates an empty stack */
+
+int checkLoad(Stack *ptrStack);
+/* CheckLoad returns:
+    -1 = stack is empty
+    0 = stack still has free space
+    1 = stack is full
+*/
